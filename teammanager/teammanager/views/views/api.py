e7cd66a823c65cb8a440afc22bb36192e0ed71ef
Zbigniew Braniecki <gandalf@mozilla.com>
2012-08-17 04:13:01 +0200

    remove old stuff

--- teammanager/teammanager/views/views/api.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from teammanager.views.utils import get_view_dependency_tree
-import json
-
-def view_graph(request, vid):
-    l = get_view_dependency_tree(vid)
-    response = json.dumps(l)
-    return HttpResponse(response, mimetype="application/json")
-
