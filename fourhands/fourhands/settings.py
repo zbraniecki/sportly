cb53e73d322e1081bfe2a642375b43892d10e5d1
Zbigniew Braniecki <gandalf@mozilla.com>
2012-10-14 00:53:51 +0200

    update blog view

--- fourhands/fourhands/settings.py
+++ fourhands/fourhands/settings.py
@@ -211,7 +211,7 @@ LANGUAGES = [
     ('pl', 'Polish'),
 ]
 
-CMSPLUGIN_BLOG_PLACEHOLDERS = ('first', 'second', 'third')
+CMSPLUGIN_BLOG_PLACEHOLDERS = ('lead', 'content')
 
 JQUERY_JS = 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js'
 JQUERY_UI_JS = 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js'
